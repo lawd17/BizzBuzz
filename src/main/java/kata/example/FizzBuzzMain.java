aa9d641458d53f9d203beb7ae3c2413f46d06adc
Jonay Hernández Izquierdo <jonayhi1717@gmail.com>
2022-03-30 09:22:28 +0100

    fix main

--- src/main/java/kata/example/FizzBuzzMain.java
+++ src/main/java/kata/example/FizzBuzzMain.java
@@ -4,7 +4,7 @@ public class FizzBuzzMain {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i < 101; i++) {
             System.out.println(fizzBuzz.obtenerResultado(i));
         }
     }
